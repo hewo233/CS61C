fc0f50d40a67fe43c6521993dd26760a7a60ced7
hewo233 <2842137843@qq.com>
2024-02-24 18:48:42 +0800

    finish lab01

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -3,5 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *slow = head, *fast = head;
+    if(head == NULL || head->next == NULL)
+    {
+        return 0;
+    }
+
+    do 
+    {
+        if(fast->next == NULL) return 0;
+        if(fast->next->next == NULL) return 0;
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return 1;
+    }while(slow != fast);
+
     return 0;
 }
