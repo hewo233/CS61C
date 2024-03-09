c5b6b74e73a956275a673a051638f0292f972f2f
hewo233 <2842137843@qq.com>
2024-03-09 14:01:13 +0800

    finish proj03

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#include "bit_ops.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    /* YOUR CODE HERE */    
+    uint16_t rb =  ((*reg & 1) ^ ( (*reg >> 2) & 1) ^ ( (*reg >> 3) & 1) ^ ((*reg >> 5) & 1));
+    *reg = *reg >> 1;
+    *reg = *reg | (rb << 15);
 }
 
---
+++ labs/test_compile/foo.h
@@ -0,0 +1,4 @@
+#ifndef FOOLIB_FOO_H_
+#define FOOLIB_FOO_H_
+void foo();
+#endif
